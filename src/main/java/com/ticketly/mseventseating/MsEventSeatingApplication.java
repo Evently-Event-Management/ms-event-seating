ba2f1ac8deb1c629af42a96319708c4ef37ad416
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-07-20 19:52:20 +0530

    initialize project structure with Spring Boot and Maven configuration

---
+++ src/main/java/com/ticketly/mseventseating/MsEventSeatingApplication.java
@@ -0,0 +1,13 @@
+package com.ticketly.mseventseating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventSeatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEventSeatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketly/mseventseating/MsEventSeatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketly.mseventseating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventSeatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
