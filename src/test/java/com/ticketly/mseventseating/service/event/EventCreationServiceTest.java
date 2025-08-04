5903013530d8022e8e1df24fdb3e34a06e2bc18a
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-08-04 23:31:16 +0530

    Remove unused import from EventCreationServiceTest to improve code clarity

--- src/test/java/com/ticketly/mseventseating/service/event/EventCreationServiceTest.java
+++ src/test/java/com/ticketly/mseventseating/service/event/EventCreationServiceTest.java
@@ -2,7 +2,6 @@ package com.ticketly.mseventseating.service.event;
 
 import com.ticketly.mseventseating.config.AppLimitsConfig;
 import com.ticketly.mseventseating.dto.AppConfigDTO;
-//import com.ticketly.mseventseating.dto.EventLimitsDTO;
 import com.ticketly.mseventseating.dto.event.CreateEventRequest;
 import com.ticketly.mseventseating.dto.event.EventResponseDTO;
 import com.ticketly.mseventseating.dto.session.SessionRequest;
